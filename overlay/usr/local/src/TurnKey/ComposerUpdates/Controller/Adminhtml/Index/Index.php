26bed0c26f0d1ec18e51f11d292ccc602b4b0f8c
Anton Pyrogovskyi <anton@turnkeylinux.org>
2016-09-04 19:09:09 +0000

    Add Composer updates enabler to the admin panel

---
+++ overlay/usr/local/src/TurnKey/ComposerUpdates/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,57 @@
+<?php
+namespace TurnKey\ComposerUpdates\Controller\Adminhtml\Index;
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory;
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;        
+        return parent::__construct($context);
+    }
+    
+    public function execute()
+    {
+        $page = $this->resultPageFactory->create();  
+        $page->setActiveMenu('TurnKey_ComposerUpdates::a_menu_item');
+        $page->getConfig()->getTitle()->prepend(__('Enable Composer Updates'));
+
+        $pubkey = $this->getRequest()->getParam('pubkey');
+        $privkey = $this->getRequest()->getParam('privkey');
+
+        if (! $pubkey || ! $privkey) {
+            return $page;
+        }
+
+        $messageBlock = $page->getLayout()->createBlock(
+            'Magento\Framework\View\Element\Messages',
+            'notification'
+        );
+
+        $outfile = fopen('/var/www/magento/auth.json', 'w');
+
+        if ($outfile) {
+            fwrite($outfile, '{"http-basic": {"repo.magento.com": {"username": "' . $pubkey .  '", "password": "' . $privkey . '"}}}');
+            fclose($outfile);
+
+            $messageBlock->addSuccess('Your keys were successfully saved at /var/www/magento/auth.json. This TurnKey Magento installation can now be upgraded with Composer.');
+        } else {
+            $messageBlock->addError('Encountered an error trying to write to /var/www/magento/auth.json. Perhaps the permissions are not www-data:www-data.');
+        }
+
+        $page->getLayout()->setChild(
+            'content',
+            $messageBlock->getNameInLayout(),
+            'notification_alias'
+        );
+
+        return $page;
+    }
+
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('TurnKey_ComposerUpdates::menu_1');
+    }
+        
+}
